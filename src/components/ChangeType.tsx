786581ce2a5b8258c12db48576f8577641516f80
Hamad <aljneibi@udel.edu>
2026-09-13 21:29:49 -0400

    state task

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,29 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question",
+    );
+
+    return (
+        <div>
+            <span>
+                {questionType === "short_answer_question" ?
+                    "Short Answer"
+                :   "Multiple Choice"}
+            </span>
+
+            <Button
+                onClick={() => {
+                    setQuestionType(
+                        questionType === "short_answer_question" ?
+                            "multiple_choice_question"
+                        :   "short_answer_question",
+                    );
+                }}
+            >
+                Change Type
+            </Button>
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,37 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    function startQuiz(): void {
+        setAttempts(attempts - 1);
+        setInProgress(true);
+    }
+
+    function stopQuiz(): void {
+        setInProgress(false);
+    }
+
+    function mulligan(): void {
+        setAttempts(attempts + 1);
+    }
+
+    return (
+        <div>
+            <span>Attempts: {attempts}</span>
+
+            <Button onClick={startQuiz} disabled={inProgress || attempts === 0}>
+                Start Quiz
+            </Button>
+
+            <Button onClick={stopQuiz} disabled={!inProgress}>
+                Stop Quiz
+            </Button>
+
+            <Button onClick={mulligan} disabled={inProgress}>
+                Mulligan
+            </Button>
+        </div>
+    );
 }
